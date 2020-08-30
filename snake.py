bac223e6e40588197a7d6d3a62066b029f577319
CristianNic <cristian.niculescu@gmail.com>
2020-08-30 15:50:21 -0700

    add escape key

--- snake.py
+++ snake.py
@@ -9,22 +9,26 @@ class Snake():
 class Food():
     def __init__(self):
         pass
-    
-    def random_position(self):
-        pass
         
-    def draw():
+    def random_position(self):
+        pass 
+        
+    def draw(self, surface):
         pass
 
 def drawGrid():
     pass 
 
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
 def main():
     pygame.init()
-            
+    
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode([500, 500])
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.fill((255, 255, 255))
+            
     snake = Snake()
     food = Food()
     
@@ -34,7 +38,11 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                    
         pygame.display.update()
         
 main()

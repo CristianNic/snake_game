355b85f2ac8d5120327f98a65a4a10cfc6f8791f
CristianNic <cristian.niculescu@gmail.com>
2020-08-31 16:03:01 -0700

    move snake

--- snake.py
+++ snake.py
@@ -12,23 +12,76 @@ screen_height = 520
 
 gridsize = 20
 
+up = (0,-1)
+down = (0,1)
+left = (-1,0)
+right = (1,0)
+    
 class Snake():
     def __init__(self):
-        self.position = (0,0)
+        self.length = 1
         self.color = green
-        self.random_position()
-        self.score = 0 
+        self.score = 0         
+        self.position = [((screen_width/2), (screen_height/2))]
+        self.direction = random.choice([up, down, left, right])
+        
+    def get_head_position(self):
+        return self.position[0]
 
-    def random_position(self):
-        # Random number between 0 and 520 divisible by 20
-        x = random.randrange(0, (screen_width-1), 20)
-        y = random.randrange(0, (screen_height-1), 20)
-        self.position = (x, y)
+    def turn(self, point):
+        if self.length > 1 and (point[0]*-1, point[1]*-1) == self.direction:
+            return
+        else:
+            self.direction = point
+        
+    def move(self):
+        current_head = self.get_head_position()
+        x,y = self.direction 
+        #head_x = ((current_head[0]+(x*gridsize))%screen_width)
+        #head_y = ((current_head[1]+(y*gridsize))%screen_length)
+        #new_head = (head_x, head_y)
+        new = (((current_head[0]+(x*gridsize))%screen_width), (current_head[1]+(y*gridsize))%screen_height)
+        if len(self.position) > 2 and new in self.position[2:]:
+            self.reset()
+        else:
+            self.position.insert(0,new)
+            if len(self.position) > self.length:
+                self.position.pop()
 
     def draw(self, surface):
-        rect = ((self.position[0], self.position[1]), (gridsize, gridsize))
-        pygame.draw.rect(surface, self.color, rect)
-
+        for p in self.position:
+            rect = pygame.Rect((p[0], p[1]), (gridsize,gridsize))
+            pygame.draw.rect(surface, self.color, rect)
+            # pygame.draw.rect(surface, (93,216, 228), r, 1)
+        
+        #rect = ((self.position[0], self.position[1]), (gridsize, gridsize))
+        #pygame.draw.rect(surface, self.color, rect)
+                    
+    def handle_keys(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()                
+                elif event.key == pygame.K_UP:
+                    self.turn(up)
+                elif event.key == pygame.K_DOWN:
+                    self.turn(down)
+                elif event.key == pygame.K_LEFT:
+                    self.turn(left)
+                elif event.key == pygame.K_RIGHT:
+                    self.turn(right)
+    
+    def reset(self):
+        self.length = 1
+        #self.position = random_position()
+        self.position = [((screen_width/2), (screen_height/2))]
+        self.direction = random.choice([up, down, left, right])
+        self.score = 0
+            
 class Food():
     def __init__(self):
         self.position = (0,0)
@@ -53,17 +106,7 @@ def drawGrid(surface):
         y = y + gridsize
         pygame.draw.line(surface, light_gray, (x, 0), (x, screen_height))
         pygame.draw.line(surface, light_gray, (0, y), (screen_width, y))
-
-def exit_keys():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
-
+                                                    
 def main():
     pygame.init()
     pygame.display.set_caption("Snake Game")
@@ -81,12 +124,14 @@ def main():
 
     while (True):
         clock.tick(10)
-        exit_keys()
-
         surface.fill(dark_gray)
         drawGrid(surface)
         food.draw(surface)
+        
+        snake.handle_keys()
+        snake.move()
         snake.draw(surface)
+        
         screen.blit(surface, (0,0))
 
         text = myfont.render("Score  " + str(snake.score), 1, green)

3b89594f4be98bced065d5e88a9bd9acb1ce694b
CristianNic <cristian.niculescu@gmail.com>
2020-08-30 17:20:46 -0700

    draw grid

--- snake.py
+++ snake.py
@@ -8,32 +8,51 @@ class Snake():
 
 class Food():
     def __init__(self):
-        pass
+        self.position = (0,0)
+        self.color = (255,0,0)
+        self.random_position()
         
     def random_position(self):
-        pass 
+        pass
         
     def draw(self, surface):
         pass
 
-def drawGrid():
-    pass 
+def drawGrid(surface):
+    x, y = 0, 0
+    for l in range(0, screen_width):
+        x = x + 20
+        y = y + 20
+        pygame.draw.line(surface, (40, 44, 52), (x, 0), (x, screen_height))
+        pygame.draw.line(surface, (40, 44, 52), (0, y), (screen_width, y))   # light gray (40, 44, 52),  ligther gray (91, 98, 111)
 
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
+screen_width = 520
+screen_height = 520
+
+gridsize = 20
+grid_width = screen_width/gridsize
+grid_height = screen_height/gridsize
+
 
 def main():
     pygame.init()
+    pygame.display.set_caption("Snake Game")
     
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill((255, 255, 255))
-            
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    x = screen.fill((33, 37, 43))
+    
+    surface = pygame.Surface(screen.get_size())
+    surface = surface.convert()
+      
     snake = Snake()
     food = Food()
     
     while (True):
         clock.tick(10)
+        drawGrid(surface)
+        screen.blit(surface, (0,0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -42,7 +61,10 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
+        
+        food.draw(surface)
                     
         pygame.display.update()
+
         
 main()
